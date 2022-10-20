2989af4ae32904f87bf6efaa40ddc1f7e5b4012a
luishenriquemota <luishenriquemota21@gmail.com>
2022-10-20 13:43:36 -0300

    :sparkles: create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1 className="heading-1-700">ola</h1>
+    <h2>ola</h2>
+    <h3 className="heading-7-500">ola</h3>
+    <p>Teste font</p>
+  </div>;
 }
 
 export default App;
