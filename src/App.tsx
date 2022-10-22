3b449ff5a400f0a9ea8fb2874292f241ca103c8c
lucascortesc <ccordeiro.lucas@gmail.com>
2022-10-22 19:52:18 -0300

    :sparkles: modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { Button } from "./Components/Button";
+import { Modal } from "./Components/Modal";
 
 function App() {
+  const [openModal, setOpenModal] = useState<boolean>(false);
   return (
     <div className="App">
-      <Button desing={"brand1"} onClick={() => console.log("Text Button")} className="button_big-text">
-        Text Button
+      <Button
+        onClick={() => setOpenModal(true)}
+        className={"button_big-text"}
+        desing={"brand1"}
+      >
+        Open Modal
       </Button>
+      {openModal && (
+        <Modal setOpenModal={setOpenModal} title={"Criar anuncio"}>
+          Modal Body
+        </Modal>
+      )}
     </div>
   );
 }
