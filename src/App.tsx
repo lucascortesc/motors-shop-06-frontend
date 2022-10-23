d10b0f4aa0ccffea9bd4668466334e9faf15718b
Amanda Oliveira <amandaolits@gmail.com>
2022-10-23 18:19:02 -0300

    :sparkles: created and styled product card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { Button } from "./Components/Button";
+import { ProductCard } from "./Components/ProductCard";
 
 function App() {
   return (
     <div className="App">
-      <Button desing={"brand1"} onClick={() => console.log("Text Button")} className="button_big-text">
+      <Button
+        desing={"brand1"}
+        onClick={() => console.log("Text Button")}
+        className="button_big-text"
+      >
         Text Button
       </Button>
+
+      <ProductCard
+        carDescription={
+          "fnwej ekfmew enfirfmf fniewfrnmf nefrgnwim fnieqemfnc iefmniemn nfiewn"
+        }
+        carKm={32}
+        carTitle={"Ford Ka"}
+        carPrice={55000}
+        carYear={2019}
+        carImage={
+          "https://http2.mlstatic.com/D_NQ_NP_976319-MLB41007342936_032020-O.jpg"
+        }
+      />
     </div>
   );
 }
---
+++ src/Components/ProductCard/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  CardFooter,
+  Container,
+  ContainerTags,
+  Description,
+  Figure,
+  Price,
+  Title,
+  Button,
+} from "./styles";
+
+
+interface IPropsProductCard{
+  carTitle: string;
+  carDescription: string;
+  carPrice: number;
+  carKm: number;
+  carYear: number;
+  carImage: string;
+  [props: string]: any;
+}
+
+export const ProductCard: React.FC<IPropsProductCard> = (props) => {
+  const {
+    carDescription,
+    carKm,
+    carTitle,
+    carPrice,
+    carYear,
+    carImage,
+  } = props;
+
+  return (
+    <Container>
+      <Figure>
+        <img src={carImage} alt={carTitle} />
+      </Figure>
+      <Title>{carTitle}</Title>
+      <Description>{carDescription}</Description>
+      <CardFooter>
+        <ContainerTags>
+          <Button>
+            {carKm} KM
+          </Button>
+          <Button>
+            {carYear}
+          </Button>
+        </ContainerTags>
+        <Price>R$ {carPrice},00</Price>
+      </CardFooter>
+    </Container>
+  );
+};
