bb223fb466a9a2b797c8a7f517518898d21ab305
lucascortesc <ccordeiro.lucas@gmail.com>
2022-11-01 03:33:32 -0300

    :sparkles: create Announcement form

--- src/Components/CreateAnnouncement/index.tsx
+++ src/Components/CreateAnnouncement/index.tsx
@@ -4,31 +4,40 @@ import { Modal } from "../Modal";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { StyledForm } from "./styles";
+import { Input } from "../Input";
+import { createAnnouncementSchema } from "../../Validation";
 
 interface Props {
   setOpenModal: (arg: boolean) => void;
 }
 
 interface schemaProps {
-  [props: string]: any;
+  title: string;
+  year: number;
+  km: number;
+  price: number;
+  description: string;
+  imgFront: string;
+  [value: string]: any;
 }
-
-const testSchema = yup.object().shape({
-  test1: yup.string().required(),
-  test2: yup.string().required(),
-  test3: yup.string().required(),
-});
 
 export const CreateAnnouncement: React.FC<Props> = ({ setOpenModal }) => {
   const [announcementType, setAnnouncementType] = useState<String>("sell");
+  const [vehicleType, setVehicleType] = useState<String>("car");
+  const [imgQuantity, setImgQuantity] = useState<number[]>([1]);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<schemaProps>({ resolver: yupResolver(testSchema) });
+  } = useForm<schemaProps>({
+    resolver: yupResolver(createAnnouncementSchema),
+    mode: "onSubmit",
+    context: { imgQuantity: imgQuantity },
+  });
 
-  const handleSetType = (button: HTMLElement) => {
+  const handleSetAnnouncementType = (button: HTMLElement) => {
     const announcementButtonType: string = button.id;
     if (announcementButtonType === "sell") {
       setAnnouncementType("sell");
@@ -37,16 +46,27 @@ export const CreateAnnouncement: React.FC<Props> = ({ setOpenModal }) => {
     }
   };
 
+  const handleSetVehicleType = (button: HTMLElement) => {
+    const vehicleButtonType: string = button.id;
+    if (vehicleButtonType === "car") {
+      setVehicleType("car");
+    } else {
+      setVehicleType("motorcycle");
+    }
+  };
+
   return (
     <Modal title="Criar anuncio" setOpenModal={setOpenModal}>
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
-        <p className="body-2-500">Tipo de anuncio</p>
-        <div>
+      <StyledForm onSubmit={handleSubmit((data) => console.log(data))}>
+        <p className="body-2-500 button__types">Tipo de anuncio</p>
+        <div className="button__wrap">
           <Button
             className="button_big-text"
             desing={announcementType === "sell" ? "brand1" : "outline2"}
             id={"sell"}
-            onClick={(e) => handleSetType(e.target as HTMLElement)}
+            onClick={(e) => handleSetAnnouncementType(e.target as HTMLElement)}
+            width="228px"
+            type="button"
           >
             Venda
           </Button>
@@ -54,13 +74,121 @@ export const CreateAnnouncement: React.FC<Props> = ({ setOpenModal }) => {
             className="button_big-text"
             desing={announcementType === "sell" ? "outline2" : "brand1"}
             id={"auction"}
-            onClick={(e) => handleSetType(e.target as HTMLElement)}
+            onClick={(e) => handleSetAnnouncementType(e.target as HTMLElement)}
+            width="228px"
+            type="button"
           >
             Leilão
           </Button>
         </div>
         <p className="body-2-500">Informações do veículo</p>
-      </form>
+        <Input
+          label="Título"
+          register={register("title")}
+          error={errors.title}
+          placeholder="Digitar título"
+        />
+        <div className="createAnnouncement__inputs-wrap">
+          <Input
+            label="Ano"
+            register={register("year")}
+            error={errors.year}
+            placeholder="Digitar ano"
+            width="small"
+          />
+          <Input
+            label="Quilometragem"
+            register={register("km")}
+            error={errors.km}
+            placeholder="0"
+            width="small"
+          />
+          <Input
+            label="Preço"
+            register={register("price")}
+            error={errors.price}
+            placeholder="Digitar preço"
+            width="small"
+            className="createAnnouncement__inputs-mobile"
+          />
+        </div>
+        <Input
+          label="Descrição"
+          register={register("description")}
+          error={errors.description}
+          placeholder="Digitar descrição"
+          type="textArea"
+        />
+        <p className="body-2-500 button__types">Tipo de veículo</p>
+        <div className="button__wrap">
+          <Button
+            className="button_big-text"
+            desing={vehicleType === "car" ? "brand1" : "outline2"}
+            id={"car"}
+            onClick={(e) => handleSetVehicleType(e.target as HTMLElement)}
+            width="228px"
+            type="button"
+          >
+            Carro
+          </Button>
+          <Button
+            className="button_big-text"
+            desing={vehicleType === "car" ? "outline2" : "brand1"}
+            id={"motorcycle"}
+            onClick={(e) => handleSetVehicleType(e.target as HTMLElement)}
+            width="228px"
+            type="button"
+          >
+            Moto
+          </Button>
+        </div>
+        <Input
+          label="Imagem da capa"
+          register={register("imgFront")}
+          error={errors.imgFront}
+          placeholder="Inserir URL da imagem"
+        />
+        {imgQuantity.map((position) => {
+          return (
+            <Input
+              key={`img${position}`}
+              label={`${position}° Imagem da galeria`}
+              register={register(`img${position}`)}
+              error={errors[`img${position}`]}
+              placeholder="Inserir URL da imagem"
+            />
+          );
+        })}
+
+        <button
+          className="createAnnouncement__image-add"
+          type="button"
+          onClick={() =>
+            setImgQuantity([...imgQuantity, imgQuantity.length + 1])
+          }
+          disabled={imgQuantity.length === 6}
+        >
+          Adicionar campo para imagem da galeria
+        </button>
+        <div className="createAnnouncement__submit-buttons">
+          <Button
+            className="button_big-text"
+            desing="negative"
+            type="button"
+            onClick={() => setOpenModal(false)}
+          >
+            Cancelar
+          </Button>
+          <Button
+            type="submit"
+            className="button_big-text"
+            desing="brand1"
+            // disabled={!isValid}
+          >
+            Criar anúncio
+          </Button>
+        </div>
+      </StyledForm>
     </Modal>
   );
 };
--- src/Validation/index.ts
+++ src/Validation/index.ts
@@ -1,1 +1,15 @@
 import * as yup from "yup";
+
+export const createAnnouncementSchema = yup.object().shape({
+  title: yup.string().required(),
+  year: yup.number().required(),
+  km: yup.number().required(),
+  price: yup.number().required(),
+  description: yup.string().required(),
+  imgFront: yup.string().required(),
+  img1: yup.string().required(),
+  img2: yup.string().when("imgQuantity", (imgQuantity) => {
+    console.log(imgQuantity);
+    return yup.string().required();
+  }),
+});
